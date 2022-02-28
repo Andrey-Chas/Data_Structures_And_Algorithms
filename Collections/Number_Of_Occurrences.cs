f47a6802c672a65277977f795cdc5ebdd82f5ffb
Andrey <78682663+Andrey-Chas@users.noreply.github.com>
2022-02-28 16:45:00 +0200

    Add files via upload

---
+++ Collections/Number_Of_Occurrences.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Utility;
+
+namespace Collections
+{
+    public static class Number_Of_Occurrences
+    {
+        public static void RunTheProgram()
+        {
+#if DEBUG
+            Console.SetIn(new System.IO.StreamReader("../../../Number_Of_Occurrences.txt"));
+#endif
+
+            var elements = ConsoleUtility.ReadSequenceOfElements<double>().ToList();
+
+            var numberOfOccurrences = FindOccurrencesOfTheElements(elements);
+
+            Output(numberOfOccurrences, elements);
+        }
+
+        public static IDictionary<T, int> FindOccurrencesOfTheElements<T>(List<T> collection)
+        {
+            var dictionaryOfOccurrences = new Dictionary<T, int>();
+
+            foreach (var item in collection)
+            {
+                if (dictionaryOfOccurrences.ContainsKey(item))
+                {
+                    dictionaryOfOccurrences[item] = dictionaryOfOccurrences[item] + 1;
+                }
+
+                else
+                {
+                    dictionaryOfOccurrences[item] = 1;
+                }
+            }
+
+            return dictionaryOfOccurrences;
+        }
+
+        public static void Output<T>(IDictionary<T, int> dictionaryOfOccurrences, List<T> collcetion)
+        {
+            Console.WriteLine("Elements counted from the following array:");
+            for (int i = 0; i < collcetion.Count; i++)
+            {
+                Console.Write($"{collcetion[i]} ");
+            }
+
+            Console.WriteLine();
+
+            foreach (var pair in dictionaryOfOccurrences)
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value} time(s)");
+            }
+        }
+    }
+}
---
+++ Data_Structures_And_Algorithms/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Data_Structures_And_Algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Data Structures And Algorithms");
+        }
+
+    }
+}
