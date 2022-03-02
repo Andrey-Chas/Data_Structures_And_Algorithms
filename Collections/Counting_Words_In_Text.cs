3c17190c6b46895ed51da506a5d8a3a6de9861f2
Andrey <78682663+Andrey-Chas@users.noreply.github.com>
2022-03-02 16:16:17 +0200

    Add files via upload

---
+++ Collections/Counting_Words_In_Text.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Utility;
+
+namespace Collections
+{
+    public static class Counting_Words_In_Text
+    {
+        public static void RunTheProgram()
+        {
+
+            var contentOfTextFile = ConsoleUtility.GetFileTextContent("../../../Words.txt");
+
+            var textWithoutPunctuation = RemovePunctuation(contentOfTextFile);
+
+            var countedWords = CountWordsInText(textWithoutPunctuation);
+
+            Output(countedWords, contentOfTextFile);
+        }
+
+        public static IList<string> RemovePunctuation(string text)
+        {
+            string textWithoutPunctuation = Regex.Replace(text, @"[^A-Za-z]+", " ");
+
+            string[] textAsArr = textWithoutPunctuation.ToLower().Trim().Split(' ');
+
+            return textAsArr.ToList();
+        }
+
+        public static IDictionary<string, int> CountWordsInText(IList<string> text)
+        {
+            var dictionaryOfCountedWords = new Dictionary<string, int>();
+
+            foreach (var item in text)
+            {
+                if (dictionaryOfCountedWords.ContainsKey(item))
+                {
+                    dictionaryOfCountedWords[item] = dictionaryOfCountedWords[item] + 1;
+                }
+
+                else
+                {
+                    dictionaryOfCountedWords[item] = 1;
+                }
+            }
+
+            return dictionaryOfCountedWords;
+        }
+
+        public static void Output(IDictionary<string, int> dictionaryOfCountedWords, string text)
+        {
+            var dictionaryOfCountedWordsSorted = dictionaryOfCountedWords.OrderBy(x => x.Value);
+
+            Console.WriteLine("Words counted from the following text:");
+            Console.WriteLine($"{text}");
+            Console.WriteLine();
+
+            foreach (var pair in dictionaryOfCountedWordsSorted)
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
+        }
+    }
+}
